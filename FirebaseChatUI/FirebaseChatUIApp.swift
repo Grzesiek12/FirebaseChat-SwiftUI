90a4da0eb5434aa9a35d589ec96c70a936c01bad
Grzesiek12 <grz.wiczk@gmail.com>
2022-01-13 22:03:40 +0100

    Initial Commit

---
+++ FirebaseChatUI/FirebaseChatUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatUIApp.swift
+//  FirebaseChatUI
+//
+//  Created by Grzegorz Wiczkowski on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
